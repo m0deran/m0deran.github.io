010f8b129639c0ff19b34149e95eacdbdba5a164
Kai Gay <gayk@oregonstate.edu>
2020-03-26 21:27:09 +0000

    works?

--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {}, ghc ? pkgs.ghc }:
-
-pkgs.haskell.lib.buildStackProject {
-  name = "patrickdelliott.com";
-  inherit ghc;
-  buildInputs = with pkgs; [ zlib ];
-  LANG = "en_US.UTF-8";
-  TMPDIR = "/tmp";
-}
